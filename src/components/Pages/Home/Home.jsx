e336ebabf0cf4d9d8da3f0d824a2012a591d18f1
jncd-v <jncdev@protonmail.com>
2021-12-14 23:09:06 +0100

    fixing layout and changing styles, working on home page and search

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import home1 from "../../../img/rect-home/home1.png";
-import home2 from "../../../img/rect-home/home2.png";
-import home3 from "../../../img/rect-home/home3.png";
-import home4 from "../../../img/rect-home/home4.png";
 import SearchBar from "./SearchBar";
 
 const Home = ({ items }) => {
@@ -14,34 +10,28 @@ const Home = ({ items }) => {
           shop all film
           <i className="far fa-arrow-alt-circle-right home__item--arrow"></i>
         </Link>
-
-        <Link className="home__item" to="/shop/category/color/black-and-white">
-          {/* <img
-            src={home1}
-            className="home__item--img"
-            alt="black and white film"
-          /> */}
+        <div className="home__main">
+          {/* <Link className="home__item" to="/shop/category/color/black-and-white">
+          <img src={{}} className="home__item--img" />
           <p className="home__item--title">black and white</p>
         </Link>
 
         <Link className="home__item" to="/shop/category/color/color-all">
-          {/* <img src={home2} className="home__item--img" alt="color film" /> */}
+          <img src={{}} className="home__item--img" />
           <p className="home__item--title">color & slide</p>
         </Link>
 
         <Link className="home__item" to="/shop/category/format/35mm">
-          {/* <img src={home3} className="home__item--img" alt="35mm film" /> */}
+          <img src={{}} className="home__item--img" />
           <p className="home__item--title">35mm</p>
         </Link>
 
         <Link className="home__item" to="/shop/category/format/120">
-          {/* <img src={home4} className="home__item--img" alt="120 film" /> */}
+          <img src={{}} className="home__item--img" />
           <p className="home__item--title">medium format</p>
-        </Link>
-
-        <div className="home__item">
-          <SearchBar items={items} />
+        </Link> */}
         </div>
+        <SearchBar items={items} />
       </div>
     </div>
   );
--- src/components/Pages/Home/SearchBar.jsx
+++ src/components/Pages/Home/SearchBar.jsx
@@ -1,95 +1,108 @@
-import React, { useState, useRef } from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState, useRef } from "react";
+import { Link } from "react-router-dom";
 
-const SearchBar = ( {items} ) => {
+const SearchBar = ({ items }) => {
+  const [searchValue, setSearchValue] = useState("");
+  const [active, setActive] = useState(false);
 
-    const [searchValue, setSearchValue] = useState('')
-    const [active, setActive] = useState(false)
+  const searchField = useRef();
 
-    const searchField = useRef()
+  const stringifyItems = items.map((item, i) => {
+    return {
+      string: (item.brand + " " + item.title).toLowerCase(),
+      index: i,
+      uniqueId: item.uniqueId,
+    };
+  });
 
-    const stringifyItems = items.map((item, i) => {
-        return {string: (item.brand + ' ' + item.title).toLowerCase(),
-                index: i,
-                uniqueId: item.uniqueId}
-        })
+  const searchItems = (array, search) => {
+    if (search !== "") {
+      const filtered = array.filter((item) =>
+        item.string.includes(search.toLowerCase())
+      );
+      return filtered.map((item) => items[item.index]);
+    } else {
+      return [];
+    }
+  };
 
-    const searchItems = (array, search) => {
-        if (search !== '') {
-            const filtered = array.filter((item) => item.string.includes(search.toLowerCase()))
-            return filtered.map((item) => items[item.index])
-        } else {
-            return [];
+  const filteredItems = searchItems(stringifyItems, searchValue);
+
+  const searchListen = () => {
+    setSearchValue(searchField.current.value);
+  };
+
+  const searchFocus = (e) => {
+    setActive(true);
+    e.target.select();
+  };
+
+  const searchBlur = () => {
+    setActive(false);
+    searchField.current.value = "";
+    setTimeout(() => {
+      setSearchValue("");
+    }, 75); //stop ul from disappearing before click event on link is registered
+  };
+
+  const transformLink = (str) => {
+    str = str.replace(/\s+/g, "-").toLowerCase();
+    return str;
+  };
+
+  return (
+    <div className="home__item searchbar">
+      <label className="search__label"></label>
+      <input
+        className={!active ? "search__input" : "search__input active"}
+        onFocus={searchFocus}
+        onChange={searchListen}
+        onBlur={searchBlur}
+        ref={searchField}
+        type="text"
+        placeholder={!active ? "search" : ""}
+      />
+
+      <div className="search__icon">
+        {!active ? (
+          <i className="fas fa-search search__icon--inactive"></i>
+        ) : (
+          <i class="fas fa-times search__icon--active"></i>
+        )}
+      </div>
+      {/* !active && searchValue conditional is to stop ul from disappearing before click event on link is registered */}
+      <ul
+        className={
+          !active && searchValue == ""
+            ? "search-results__list"
+            : "search-results__list active"
         }
-    }
-
-    const filteredItems = searchItems(stringifyItems, searchValue)
-
-    const searchListen = () => {
-        setSearchValue(searchField.current.value)
-    }
-
-    const searchFocus = (e) => {
-        setActive(true)
-        e.target.select()
-    }
-
-    const searchBlur = () => {
-        setActive(false)
-        searchField.current.value = ''
-        setTimeout(() => { setSearchValue('')}, 75) //stop ul from disappearing before click event on link is registered
-    }
-
-    const transformLink = (str) => {
-        str = str.replace(/\s+/g, '-').toLowerCase();
-        return str
-    } 
-
-    return (
-        <>
-
-            <label className="search__label"></label>
-            <input className={!active ? "search__input" : "search__input active"} 
-                onFocus={searchFocus} 
-                onChange={searchListen} 
-                onBlur={searchBlur} 
-                ref={searchField} 
-                type="text" 
-                placeholder={!active ? "search" : ""} 
-                />
-            
-            <div className="search__icon">
-            {!active 
-            ?
-            <i className="fas fa-search search__icon--inactive"></i>
-            : 
-            <i class="fas fa-times search__icon--active"></i>
-            }
-            </div>
-            {/* !active && searchValue conditional is to stop ul from disappearing before click event on link is registered */}
-            <ul className={!active && searchValue == '' ? "search-results__list" : "search-results__list active"}>
-                {filteredItems.length > 0  
-                ? 
-                filteredItems.map((item) => 
-
-                <Link className="search__item" to={`/shop/item/${item.format}/${transformLink(item.title)}`}>
-                        <div className="search__text">
-                            <h3 className="search__text--brand text">{item.brand}</h3>
-                            <h4 className="search__text--title text">{item.title}</h4>
-                        </div>
-                        <div className="search__text">
-                            <p className="search__text--format text">format: {item.format}</p>
-                            <p className="search__text--type text">{item.type.toLowerCase()}</p>
-                            <p className="search__text--iso text">{item.iso} iso</p>
-                        </div>
-                </Link>)
-
-                : 
-                null
-                }
-            </ul>
-        </>
-    )
-}
+      >
+        {filteredItems.length > 0
+          ? filteredItems.map((item) => (
+              <Link
+                className="search__item"
+                to={`/shop/item/${item.format}/${transformLink(item.title)}`}
+              >
+                <div className="search__text">
+                  <h3 className="search__text--brand text">{item.brand}</h3>
+                  <h4 className="search__text--title text">{item.title}</h4>
+                </div>
+                <div className="search__text">
+                  <p className="search__text--format text">
+                    format: {item.format}
+                  </p>
+                  <p className="search__text--type text">
+                    {item.type.toLowerCase()}
+                  </p>
+                  <p className="search__text--iso text">{item.iso} iso</p>
+                </div>
+              </Link>
+            ))
+          : null}
+      </ul>
+    </div>
+  );
+};
 
 export default SearchBar;
